a552ed9534ee774ae2e703eaa4ce1649e2c1e1ab
JacobMason83 <jmason062083@gmail.com>
2020-12-21 18:27:37 -0500

    Introduction to React Components video

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import moment from 'moment'
 
+import PortfolioContainer from './portfolio-container'
+
 const hello = 'Thanks for coming to my Page , any questions just ask';
 
 
@@ -10,6 +12,9 @@ export default class App extends Component {
     return (
       <div className='app'>
         <h1>Jacob Mason Portfolio</h1>  
+        <PortfolioContainer />
+        <PortfolioContainer />
+        <PortfolioContainer />
         <div>{hello + ' ' + moment().format('MMMM Do YYYY, h:mm:ss a')}</div>      
       </div>
     );
