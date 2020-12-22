cdd1235e308dbcd1a295acb3108efa359e0ccb43
JacobMason83 <jmason062083@gmail.com>
2020-12-22 12:50:16 -0500

    Custom Functions in React and Using Map to Loop Over Data

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -6,11 +6,24 @@ import PortfolioItem from "./portfolio-items"
 // difference between class and functional is that class can use state and lifecycle, although that has changed with hooks, but overall state and 
 // lifecycle is the reasons 
 export default class PortfolioContainer extends Component {
+    constructor(){
+        super();
+        console.log('Portfolio Container has Rendered');
+        
+    }
+// dynamic in nature and allows you to map the data and putting it on the screen no more hardcoding
+    portfolioItems() {
+        const data = ['Quip', 'Eventbrite', 'Ministry Safe' ] 
+        return data.map(item => {
+            return <PortfolioItem />
+        })
+    }
     render(){
         return(
             <div>
                 <h2>Portfolio items go here </h2>
-                <PortfolioItem />
+                
+                {this.portfolioItems()}
             </div>
         )
     }
