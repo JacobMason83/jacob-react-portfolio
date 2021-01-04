a6abb3dbc695bc803ff30164bca7b4e5638d4113
JacobMason83 <jmason062083@gmail.com>
2021-01-04 17:59:57 -0500

    How to Access URL Values in React

--- src/components/portfolio/portfolio-items.js
+++ src/components/portfolio/portfolio-items.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 // all were gonna do is pass it values, functional compents wont be as smart as a class / presentational component
 
@@ -8,6 +8,8 @@ export default function(props) {
         <div>
         <h3>{props.title} </h3>
         <h4>{props.url}</h4>
+
+        <Link to={`/portfolio/${props.slug}`}>Link</Link>
         </div>
     )
 }
