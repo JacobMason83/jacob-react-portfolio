ccac3437067456bbd13f384dbbcde2ea6037d3f0
JacobMason83 <jmason062083@gmail.com>
2021-01-05 18:35:10 -0500

    How to Render Images in React from an API

--- src/components/portfolio/portfolio-items.js
+++ src/components/portfolio/portfolio-items.js
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom'
 // all were gonna do is pass it values, functional compents wont be as smart as a class / presentational component
 
 export default function(props) {
-    const {id, description, thumb_image_url, logo} = props.item
+    const {id, description, thumb_image_url, logo_url} = props.item
     return (
         <div>
-      
+        <img src={thumb_image_url}/>
+        <img src={logo_url}/>
+        <div>{description}</div>
+        <h4>{props.url}</h4>
 
         <Link to={`/portfolio/${id}`}>Link</Link>
         </div>
