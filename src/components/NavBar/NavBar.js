6caed741751594b549209be238a62ee993bf45c3
JacobMason83 <jmason062083@gmail.com>
2021-01-06 16:37:36 -0500

    Building the Navigation Link Wrapper Styles

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,29 +8,28 @@ export default class NavBar extends Component {
     render() {
         console.log(this.props)
         return (
-            <div className="nav-wrapper"> 
-            <div className="left-side" >             
-            <NavLink exact to="/" activeClassName="nav-link-active">
-                Home
-            </NavLink> 
-            <NavLink to="about-me">
-                About
-            </NavLink>
-            <NavLink to="contact">
-                Contact
-            </NavLink>
-            <NavLink to="blog">
-                Blog
-            </NavLink>
+<div className='nav-wrapper'>
+  <div className='left-side'>
+    <div className='nav-link-wrapper'>
+      <NavLink exact to='/' activeClassName='nav-link-active'>
+        Home
+      </NavLink>
+    </div>
+    <div className='nav-link-wrapper'>
+      <NavLink to='about-me'>About</NavLink>
+    </div>
+    <div className='nav-link-wrapper'>
+      <NavLink to='contact'>Contact</NavLink>
+    </div>
+    <div className='nav-link-wrapper'>
+      <NavLink to='blog'>Blog</NavLink>
+    </div>
 
+    {false ? <button>Add Blog</button> : null}
+  </div>
+  <div className='right-side'>JACOB MASON</div>
+</div>
 
-               
-                {false ? <button>Add Blog</button> : null} 
-                </div>
-                <div className="right-side">
-                    JACOB MASON
-                </div>
-            </div>
         )
     }
 }// using an ternary operator to show if the button is hidden or not
