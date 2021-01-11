1949383285278b8a4bb79841db4ada1e78c7f232
JacobMason83 <jmason062083@gmail.com>
2021-01-10 19:32:32 -0500

    populating the portfolio manager state with api call data

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 
 export default class PortfolioManager extends Component {
   constructor () {
     super()
+    this.state = {
+       portfolioItems: []
+    }
+  }
+
+  getPortfolioItems = () => {
+     axios
+     .get('https://jacobmason.devcamp.space/portfolio/portfolio_items', {withCredentials: true})
+     .then(res => {
+        this.setState({
+           portfolioItems: [...res.data.portfolio_items]
+          })
+     })
+     .catch(err => console.error(err))
+  }
+  componentDidMount() {
+     this.getPortfolioItems()
   }
 
   render () {
