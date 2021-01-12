aaa5401b1c931b7d993e36d25a11b30b44ac776a
JacobMason83 <jmason062083@gmail.com>
2021-01-12 18:22:14 -0500

    added blog fetch call to get the items from the api

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
  export default class Blog extends Component {
      constructor(){
          super()
 
+         this.state ={
+             blogItems: []
+         }
+     }
+     getBlogItems = () => {
+         axios
+         .get('https://jacobmason.devcamp.space/portfolio/portfolio_blogs', {withCredentials: true})
+         .then(res => {
+             this.setState({
+                 blogItems: res.data.portfolio_blogs
+             })
+         })
+         .catch(err => console.error(err))
+     }
+     componentWillMount() {
+         this.getBlogItems()
      }
     
     render() {
@@ -13,6 +30,7 @@ import { Link } from 'react-router-dom'
 
         <div>
             <Link to='/about-me'> Read more about myself </Link>
+            
             </div>
         </div>
     )
