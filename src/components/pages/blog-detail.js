0e1965b7b4e96a91581c9a044d3d463d41fdda01
JacobMason83 <jmason062083@gmail.com>
2021-01-13 13:26:00 -0500

    calling a single blog item from the api

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class BlogDetail extends Component {
-  constructor () {
-    super()
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentId: this.props.match.params.slug
+    }
   }
 
+  getBlogItem = () => {
+    axios
+    .get(`https://jacobmason.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`, {withCredentials: true})
+    .then(res => {
+      console.log("res from getBlogItem", res)
+    })
+    .catch(err => console.error("blogitemError", err))
+  }
+
+
   render () {
+    console.log(this.state.currentId)
     return (
       <div>
         <h1>Blog detail</h1>
