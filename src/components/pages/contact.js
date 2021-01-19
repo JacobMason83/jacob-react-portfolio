379dd404db00cb05224da93a950bd1a76cca0c16
JacobMason83 <jmason062083@gmail.com>
2021-01-19 13:06:13 -0500

    Building Contact Page Styles for the React Portfolio

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import contactPicture from '../../../static/assets/images/bio/contact-pic.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export default function(){
     return(
-        <div>
-         Contact
+        <div className="content-page-wrapper">
+        <div 
+        className="left-column"
+        style={{
+            background:"url(" + contactPicture + ") no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center"
+        }}
+         />
+        <div className="right-side">
+            <div className="contact-bullet-points">
+                <div className="bullet-point-group">
+                    <div className="icon"><FontAwesomeIcon icon="phone" /> </div>
+                    <div className="text">260-226-9640</div>
+        </div>
+                <div className="bullet-point-group">
+                    <div className="icon"><FontAwesomeIcon icon="envelope" /> </div>
+                    <div className="text">neozane23@gmail.com</div>
+            </div>
+                <div className="bullet-point-group">
+                    <div className="icon"><FontAwesomeIcon icon="map-marked-alt" /> </div>
+                    <div className="text">Garret Indiana </div>
+                </div>
+         
+        </div>
+        </div>
         </div>
     )
 }
