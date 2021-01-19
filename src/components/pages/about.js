fc71fc135f1a7fe4fb5fa9d08e6de3108b5a347c
JacobMason83 <jmason062083@gmail.com>
2021-01-19 12:26:01 -0500

    building about page styles for react portfolio

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import profilePicture from '../../../static/assets/images/bio/bio-pic.jpg'
 export default function(){
     return(
-        <div>
-            About
+        <div className="content-page-wrapper">
+        <div 
+        className="left-column"
+        style={{
+            background:"url(" + profilePicture + ") no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center"
+        }}
+         />
+         
+        <div className="right-side">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit dolorem, soluta praesentium aperiam a nostrum illo eveniet. Voluptas illo et dignissimos aspernatur? Corrupti ut rerum nulla voluptate temporibus consequuntur non!
+            Autem perferendis asperiores aperiam iusto excepturi laboriosam quo vero distinctio, blanditiis expedita ipsa, dignissimos atque inventore eos, sapiente tempore doloremque. Nam quibusdam rerum saepe tenetur unde eos ipsa accusantium libero.
+            Quaerat illum asperiores consectetur a id voluptatibus magni, possimus impedit. Quidem qui distinctio dolorum minima delectus culpa omnis. Quaerat ipsam voluptatibus officia, omnis ad voluptas eius provident nostrum atque! Voluptates!
+            Laboriosam enim quas error iusto, magni in eaque nemo qui debitis itaque delectus libero. Dignissimos inventore porro a ipsum excepturi suscipit numquam, amet quo, quam quod possimus quos consectetur sed.
+            A eaque expedita dolorum, aut repellat fugiat. Iste, quae quibusdam cumque voluptatem harum maiores! Quis aliquid fuga dolorem officiis. Neque porro ab perferendis suscipit non quae a recusandae incidunt est.
+            Perspiciatis voluptatibus, fugiat quia quae in voluptatem. In, commodi laudantium voluptas rerum enim maxime maiores doloremque atque adipisci. Officia et voluptatibus cumque labore eveniet adipisci exercitationem rem eaque consequuntur asperiores.
+            Quibusdam nulla accusamus quos soluta iusto praesentium debitis laborum non! Dolorum voluptas qui earum iste ducimus? Esse illo repudiandae, non, possimus veritatis error quo debitis ab dolor omnis necessitatibus asperiores?
+            Deleniti ea sit quidem praesentium quo numquam dolor sint consequatur dignissimos, eaque labore a suscipit odio eos consequuntur dolorum architecto et nisi? Nihil laborum ullam voluptatibus, facere culpa dicta iusto.</p>
+        </div>
+            
         </div>
     )
 }
