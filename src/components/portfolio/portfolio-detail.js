3e41def20c92f5cd30f219a666a7b4a3ef620bf2
JacobMason83 <jmason062083@gmail.com>
2021-01-19 13:51:49 -0500

    got the portfolio detail page getting data and now setting up styles for the page

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -4,24 +4,41 @@ import React, { Component } from 'react'
 export default class PortfolioDetail extends Component{
     constructor(props) {
         super(props)
+        this.state = {
+            portfolioItem: {}
+        }
     }
     getPortfolioItem = () =>{
         axios
         .get(`https://jacobmason.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`, {withCredentials: true})
         .then(res => {
-            console.log("get Portfolio item ", res)
+            this.setState({
+                portfolioItem: res.data.portfolio_item
+            })
         })
         .catch(err => console.error(err))
     }  
-    componentDidMount(){
+    componentDidMount() {
         this.getPortfolioItem()
     } 
     render() {
+        const { banner_image_url, name, description, url, category, thumb_image_url, logo_url} = this.state.portfolioItem
+       
         return(
-            <div>
-             <h2>Portfolio detail for {this.props.match.params.slug} </h2>
+            <div className="portfolio-detail-wrapper">
+             <h1>{name}</h1>
+             <h2>{category}</h2>
+             <div className="images">
+             <img src={banner_image_url} />
+             <img src={logo_url} />
+             
+
+             </div>
+             <p>{description}</p>
             </div>
         )
 
     }
 }
+
+
