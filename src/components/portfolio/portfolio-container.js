aa08502cd9987cbe90232d3cf9ec740e621cdf5a
JacobMason83 <jmason062083@gmail.com>
2021-01-19 14:35:55 -0500

    Full Filtering Feature Build Out in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -37,29 +37,35 @@ export default class PortfolioContainer extends Component {
         })
     }
     handleFilter = (filter) => {
-        this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter;
-            })
-        })
-
+        if(filter === "CLEAR_FILTERS") {
+            this.getPortfolioItems()
+        } else {
+            this.getPortfolioItems(filter)
+        }
     }
-    getPortfolioItems() {
+    getPortfolioItems(filter = null) {
 
         axios
         .get('https://jacobmason.devcamp.space/portfolio/portfolio_items')
         .then((response) => {
-          // handle success
-          console.log("Response Data", response);
-          this.setState({
-              data: response.data.portfolio_items
-          })
+          if(filter){
+            this.setState({
+                data: response.data.portfolio_items.filter(item => {
+                    return item.category === filter;
+                })
+            })
+         } else {
+              this.setState({
+                  data: response.data.portfolio_items
+              })
+            }
+        
         })
         .catch((error) => {
-          // handle error
+         
           console.log(error);
         })  
-      }
+    }
       componentDidMount(){
           this.getPortfolioItems()
       }
@@ -69,13 +75,24 @@ export default class PortfolioContainer extends Component {
         }
         
         return(
-            <div className="portfolio-items-wrapper">
-                <button className="btn" onClick={() => this.handleFilter('School')}>School</button>
-                <button className="btn" onClick={() => this.handleFilter('InSchool')}>InSchool</button>
-                <button className="btn" onClick={() => this.handleFilter('MyProjects')}>MyProjects</button>
-                {this.portfolioItems()}
+           <div className='homepage-wrapper'>
+  <div className='filter-links'>
+    <button className='btn' onClick={() => this.handleFilter('School')}>
+      School
+    </button>
+    <button className='btn' onClick={() => this.handleFilter('InSchool')}>
+      InSchool
+    </button>
+    <button className='btn' onClick={() => this.handleFilter('MyProjects')}>
+      MyProjects
+    </button>
+    <button className='btn' onClick={() => this.handleFilter('CLEAR_FILTERS')}>
+      All
+    </button>
+  </div>
+  <div className='portfolio-items-wrapper'>{this.portfolioItems()}</div>
+</div>
 
-                </div>
                 
           
         )
