7daec622a6be079029a99b824c0c06a6ced9e832
JacobMason83 <jmason062083@gmail.com>
2021-02-07 16:59:48 -0500

    added about page to about.js

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -13,14 +13,25 @@ export default function(){
          />
          
         <div className="right-side">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit dolorem, soluta praesentium aperiam a nostrum illo eveniet. Voluptas illo et dignissimos aspernatur? Corrupti ut rerum nulla voluptate temporibus consequuntur non!
-            Autem perferendis asperiores aperiam iusto excepturi laboriosam quo vero distinctio, blanditiis expedita ipsa, dignissimos atque inventore eos, sapiente tempore doloremque. Nam quibusdam rerum saepe tenetur unde eos ipsa accusantium libero.
-            Quaerat illum asperiores consectetur a id voluptatibus magni, possimus impedit. Quidem qui distinctio dolorum minima delectus culpa omnis. Quaerat ipsam voluptatibus officia, omnis ad voluptas eius provident nostrum atque! Voluptates!
-            Laboriosam enim quas error iusto, magni in eaque nemo qui debitis itaque delectus libero. Dignissimos inventore porro a ipsum excepturi suscipit numquam, amet quo, quam quod possimus quos consectetur sed.
-            A eaque expedita dolorum, aut repellat fugiat. Iste, quae quibusdam cumque voluptatem harum maiores! Quis aliquid fuga dolorem officiis. Neque porro ab perferendis suscipit non quae a recusandae incidunt est.
-            Perspiciatis voluptatibus, fugiat quia quae in voluptatem. In, commodi laudantium voluptas rerum enim maxime maiores doloremque atque adipisci. Officia et voluptatibus cumque labore eveniet adipisci exercitationem rem eaque consequuntur asperiores.
-            Quibusdam nulla accusamus quos soluta iusto praesentium debitis laborum non! Dolorum voluptas qui earum iste ducimus? Esse illo repudiandae, non, possimus veritatis error quo debitis ab dolor omnis necessitatibus asperiores?
-            Deleniti ea sit quidem praesentium quo numquam dolor sint consequatur dignissimos, eaque labore a suscipit odio eos consequuntur dolorum architecto et nisi? Nihil laborum ullam voluptatibus, facere culpa dicta iusto.</p>
+            <p>Hello, I am Jacob Mason and i am a graduate of Bottega University Full Stack Web Development Certification Program. 
+I am 37 years old, and I am making a career change, into something that i have wanted to do since i first saw the matrix, 
+back in 1999. I have always been interested in computers, and how they work, and how websites are build, styled and 
+how the data is put onto the page. So back in September of 2020, I made a descision to take a huge risk , and bet it all on
+me going back to school fulltime , and hopefully changing my life, and my families life. I want to be able to provide for them, 
+like they deserve to be, and web development is a booming industry right now, and it was prime for me to make that change, and switch
+careers. Before all of this, I worked in alot of fields trying to find my niche, and I couldent find it until i got into programming. It 
+gives me a smile on my face, and an enjoyment that i never thought possible. If I struggle at anything, I work on it until i get it, or if 
+its taking me longer, ill take a break and comeback to it, and usually that little break helps. I have worked in the Resturant business, as a 
+delivery driver, manager, pizza-maker, a cook , pretty much you name it , i have done it. I also got into the medical field, helping and serving, 
+individuals with MRDD , or any developmental disability, and i enjoyed it, and i brought smiles to my individuals faces, but it wasnt what i wanted
+either. In 2017 i moved to Indiana to be near my family, and changed scenery from the big city of Toledo OH to near Fort Wayne In. There 
+i started free code camps program, but with work, family and other distractions, i could never fully grasp it all. But after i married my
+wife in July 11, 2020 , and having a family to support I knew i had to do something, and as I said earlier in September 2020, I made the 
+descision to go back to school, and now the rest is history. If you like any of my projects or want to talk about opportunities, 
+please email me at neozane23@gmail.com, or you can call or text Monday thru Friday from 9am to 9pm est. I will try and answer if I see the call, it 
+may be easier to text me first, and then call . Thanks for taking the time to read my story, and I hope to talk soon. <br />
+Thanks <br />
+Jacob D Mason</p>
         </div>
             
         </div>
